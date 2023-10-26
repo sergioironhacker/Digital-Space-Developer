5de4500622e4f487941f066eb2fb93c15a8b621c
Sergio Esteban <Sergioss772022@gmail.com>
2023-10-26 20:59:49 +0200

    fourth-commit

--- Proyecto Modulo 1/Src/index.js
+++ Proyecto Modulo 1/Src/index.js
@@ -1,14 +1,29 @@
 window.addEventListener("load", () => {
+    const startContainer = document.getElementById("background-image");
+    const startButton = document.getElementById("start-button");
     const container = document.getElementById("game-board");
-    const game = new Game(container);
+    let game = null;
+  
     
+
+   startButton.addEventListener('click', () => {
+    // Ocultar el botón una vez que se inicia el juego
+    startContainer.style.display = 'none';
+    startButton.style.display = 'none';
+
+    // Iniciar el juego solo cuando se hace clic en el botón
+    game = new Game(container);
+
 
     document.addEventListener('mousemove', (e) => {
         game.player.move(e);
        
-    })
+    });
 
     document.addEventListener('click', (e) => {
         game.player.shoot();
-    })
+    });
+
+   });
+
 });
