f7267a4fa41e20b8c1ff27d901f952ded549cd50
Sergio Esteban <Sergioss772022@gmail.com>
2023-10-27 16:43:47 +0200

    [200~

--- Src/index.js
+++ Src/index.js
@@ -2,17 +2,29 @@ window.addEventListener("load", () => {
     const startContainer = document.getElementById("background-image");
     const startButton = document.getElementById("start-button");
     const container = document.getElementById("game-board");
+    const backgroundMusic = document.getElementById("background-music");
     let game = null;
   
-    
+
+    backgroundMusic.play();
+  
 
    startButton.addEventListener('click', () => {
     // Ocultar el botón una vez que se inicia el juego
     startContainer.style.display = 'none';
     startButton.style.display = 'none';
 
+    
+
+    backgroundMusic.pause();
+
+
+
     // Iniciar el juego solo cuando se hace clic en el botón
     game = new Game(container);
+
+
+    
 
 
     document.addEventListener('mousemove', (e) => {
@@ -25,5 +37,6 @@ window.addEventListener("load", () => {
     });
 
    });
+   
 
 });
