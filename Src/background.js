018130f91008d5c268ee47cef926a4d7866018f3
usefercas <124436934+usefercas@users.noreply.github.com>
2023-10-30 09:12:15 +0100

    cambios actuales

--- Src/background.js
+++ Src/background.js
@@ -1,26 +1,24 @@
 class Background {
     constructor(container) {
-        this.container = container;
-        this.width = this.container.offsetWidth;
-        this.height = this.container.offsetheight;
+        // Constructor de la clase Background, inicializa las propiedades del fondo
+        this.container = container; // Establecer el contenedor del fondo
+        this.width = this.container.offsetWidth; // Ancho del fondo igual al ancho del contenedor
+        this.height = this.container.offsetHeight; // Alto del fondo igual al alto del contenedor
 
-        this.x = 0;
-        this.y = 0;
+        this.x = 0; // Posición horizontal inicial del fondo
+        this.y = 0; // Posición vertical inicial del fondo
 
-        this.element = document.createElement("div");
-        this.element.style.position = "absolute";
-        this.element.style.background = `url(./assets/espacio.jpeg)`;
-        this.element.style.backgroundSize = "cover";
-        this.element.style.backgroundPosition = "bottom";
-        this.element.style.width = `${this.width}px`;
-        this.element.style.height = `${this.height}px`;
-        this.element.style.left = `${this.x}px`; 
-        this.element.style.top = `${this.y}px`; 
-        
-        
+        this.element = document.createElement("div"); // Crear un elemento HTML para representar el fondo
 
-      
+        this.element.style.position = "absolute"; // Establecer posición absoluta
+        this.element.style.background = `url(./assets/espacio.jpeg)`; // Establecer una imagen de fondo
+        this.element.style.backgroundSize = "cover"; // Ajustar el tamaño del fondo
+        this.element.style.backgroundPosition = "bottom"; // Posición de la imagen de fondo
+        this.element.style.width = `${this.width}px`; // Ancho del elemento igual al ancho del fondo
+        this.element.style.height = `${this.height}px`; // Alto del elemento igual al alto del fondo
+        this.element.style.left = `${this.x}px`; // Posición horizontal del elemento
+        this.element.style.top = `${this.y}px`; // Posición vertical del elemento
 
-        this.container.appendChild(this.element);
+        this.container.appendChild(this.element); // Agregar el fondo al contenedor
     }
 }
--- Src/words.js
+++ Src/words.js
@@ -1,13 +1,18 @@
 class Words {
-    constructor(container, playerPosY, playerHeight) {
-        this.container = container;
-        this.width = 50;
-        this.height = 50;
-        this.posY = playerPosY - playerHeight;
+    constructor(container, player, playerHeight) {
+        // Constructor de la clase Words, inicializa las propiedades
+        console.log(player); // Imprimir información del jugador
 
-        this.activeWords = [];
-        this.playerPosY = playerPosY;
+        this.container = container; // Contenedor del juego
+        this.width = 50; // Ancho de las palabras
+        this.height = 50; // Alto de las palabras
+        this.posY = player - playerHeight; // Posición vertical inicial de las palabras
+        this.posX; // Posición horizontal (inicialmente indefinida)
+        this.activeWords = []; // Almacenamiento de palabras activas
+        this.player = player; // Jugador
 
+
+        // Palabras disponibles para el juego (dos listas)
         this.allWords = [
             ["variable", "funcion", "if", "else", "for", "while", "console",
                 "array", "object", "return", "true", "false", "null", "undefined",
@@ -20,62 +25,108 @@ class Words {
                 "haskell", "ironHack", "prolog"]
         ];
 
-        //  setInterval(() => this.createAndMoveWord(), 3000);
-
+        // Configuración de tiempo y velocidad de las palabras
         this.lastSpawnTime = 0;
         this.spawnInterval = 3000; // Tiempo en milisegundos entre la aparición de nuevas palabras
         this.wordSpeed = 2; // Velocidad de caída constante
 
+        // Iniciar el ciclo de actualización
         this.update();
-
     }
 
     createAndMoveWord() {
-        const randomWord = this.getRandomWord();
-        const posX = Math.random() * (this.container.offsetWidth - this.width);
-        const posY = -this.height;
-        const wordElement = this.createWordElement(randomWord, posX, posY);
+        // Crear y mover una palabra
+        const randomWord = this.getRandomWord(); // Obtener una palabra aleatoria
+        this.posX = 500; // Posición horizontal inicial de la palabra
+        const posY = -this.height; // Posición vertical inicial de la palabra
+        const wordElement = this.createWordElement(randomWord, this.posX, posY);
 
+        // Agregar la palabra al contenedor y rastrearla como palabra activa
         this.container.appendChild(wordElement);
-        this.activeWords.push({ element: wordElement, posY, speed: this.wordSpeed });
+        this.activeWords.push({ element: wordElement, posY, posX: this.posX, speed: this.wordSpeed, name: randomWord });
     }
 
     update() {
+        // Revisamos si queda tiempo
+        const remainingGameTime = parseInt(document.getElementById("timer").textContent);
+        //console.log("Queda tiempo " + remainingGameTime);
+        // if(remainingGameTime <= 0) {
+        //   return "Game Over";
+        // } 
+        // Función de actualización del juego
+        console.log("PAlabra en pantalla  " + JSON.stringify(this.activeWords));
         const currentTime = Date.now();
+
+        // Comprobar si es el momento de crear una nueva palabra
         if (currentTime - this.lastSpawnTime >= this.spawnInterval) {
-            this.createAndMoveWord();
-            this.lastSpawnTime = currentTime;
+            this.createAndMoveWord(); // Crear y mover una nueva palabra
+            this.lastSpawnTime = currentTime; // Actualizar el tiempo de la última aparición
         }
 
         for (let i = this.activeWords.length - 1; i >= 0; i--) {
             const word = this.activeWords[i];
+
+            // Mover la palabra hacia abajo
             word.posY += word.speed;
             word.element.style.top = word.posY + "px";
 
-            if (word.posY > this.playerPosY) {
-                // Comprueba si la palabra llega al jugador
-                // Aquí puedes agregar la lógica para manejar la colisión
-                // Por ejemplo, eliminar la palabra y restar puntos al jugador
-                // this.container.removeChild(word.element);
-                // this.activeWords.splice(i, 1);
+            let currentBullets = this.player.bullets;
+
+            // Comprobar si una bala golpea una palabra
+            if (currentBullets != null && currentBullets != undefined && currentBullets.length != 0) {
+                for (let counter = 0; currentBullets.length > counter; counter++) {
+                    let currentBullet = currentBullets[counter];
+                    if (currentBullet.y <= word.posY && (Math.round(currentBullet.x) - 50 <= Math.round(word.posX) &&
+                        Math.round(currentBullet.x) + 50 >= Math.round(word.posX))) {
+                        const goodWords = [...this.allWords[0]];
+                        console.log("Comparacion de palabra " + word.name);
+                        if (goodWords.includes(word.name)) {
+                            const score = document.getElementById("points").innerHTML;
+                            const newScore = parseInt(score) + 1;
+                            document.getElementById("points").innerHTML = newScore;
+                        } else {
+                            const lives = document.getElementById("live").innerHTML;
+                            const newLives = parseInt(lives) - 1;
+                            if(newLives <= 0) {
+                                document.getElementById("live").innerHTML = newLives;
+                                throw new Error("El juego acabo");  
+                            }
+                            document.getElementById("live").innerHTML = newLives;
+                        }
+
+
+                        // Eliminar la bala del jugador
+                        let indexBullet = this.player.bullets.indexOf(currentBullet);
+                        this.player.bullets.splice(indexBullet, 1);
+
+                        // Quitar la palabra del contenedor y de la lista de palabras activas
+                        this.container.removeChild(word.element);
+                        this.activeWords.splice(i, 1);
+                    }
+                }
             }
 
+            // Quitar la palabra si se ha desplazado fuera del contenedor
             if (word.posY > this.container.offsetHeight) {
                 this.container.removeChild(word.element);
                 this.activeWords.splice(i, 1);
             }
         }
 
+        // Llamar a la función 'update' nuevamente para la próxima actualización
         requestAnimationFrame(() => this.update());
     }
 
     getRandomWord() {
+        // Obtener una palabra aleatoria de las listas de palabras disponibles
         const wordSet = this.allWords[Math.floor(Math.random() * this.allWords.length)];
         return wordSet[Math.floor(Math.random() * wordSet.length)];
     }
 
     createWordElement(word, posX, posY) {
+        // Crear un elemento de palabra con estilo y texto específicos
         const wordElement = document.createElement("div");
+    
         wordElement.textContent = word;
         wordElement.style.color = "white";
         wordElement.style.fontFamily = "Arial";
@@ -86,21 +137,15 @@ class Words {
         wordElement.style.transition = "color 1s";
         wordElement.style.color = "white";
 
+        // Configuración del sombreado del texto
+        wordElement.style.textShadow = "-8px -8px 10px rgba(0, 0, 255, 0.8), 8px -8px 10px rgba(0, 0, 255, 0.8), -8px 8px 10px rgba(0, 0, 255, 0.8), 8px 8px 10px rgba(0, 0, 255, 0.8)";
 
-         //wordElement.style.textShadow = "8px 8px 10px rgba(0, 0, 255, 1)";
-         wordElement.style.textShadow = "-8px -8px 10px rgba(0, 0, 255, 0.8), 8px -8px 10px rgba(0, 0, 255, 0.8), -8px 8px 10px rgba(0, 0, 255, 0.8), 8px 8px 10px rgba(0, 0, 255, 0.8)";
-
-
+        // Cambiar el color de la palabra después de 2 segundos
         setTimeout(() => {
-        wordElement.style.color = "yellow";
+            wordElement.style.color = "yellow";
         }, 2000);
 
         return wordElement;
     }
 }
-
-
-
-
-
 
